22cd8f3395ca3f554fe2e320db61f3b516f0e089
Nikita Kaz <nik.09.05.05@gmail.com>
2024-03-12 14:49:57 +0200

    +PR_3

---
+++ PR_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Title title = new Title("Будь корисним. Сiм життєвих правил");
+            Author author = new Author("Арнольд Шварценеггер");
+            Content content = new Content("Iсторiя вiдомого бодибiлдера");
+
+            Book book = new Book(title, author, content);
+            book.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
