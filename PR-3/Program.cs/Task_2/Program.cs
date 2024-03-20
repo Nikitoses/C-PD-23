4295f3785c4f7fdd6648bfd1d526c40f21349b06
Nikita Kaz <nik.09.05.05@gmail.com>
2024-03-20 14:50:22 +0200

    +PR_3 task 2

---
+++ PR-3/Program.cs/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point A = new Point(0, 0, "A");
+        Point B = new Point(0, 1, "B");
+        Point C = new Point(1, 1, "C");
+        Point D = new Point(1, 0, "D");
+
+        Figure figure = new Figure(A, B, C, D);
+        figure.PerimeterCalculator();
+    }
+}
